9f84ac39e5ad045a155339a34c1a3b8bf7b75372
Monty __L <b21060414@njupt.edu.cn>
2025-02-06 17:18:54 +0800

    style: 添加代码注释

--- client/src/SearchBridge.cpp
+++ client/src/SearchBridge.cpp
@@ -1,3 +1,11 @@
+/*
+ * Author: Montee
+ * CreateDate: 2025-01-30
+ * UpdateDate: 2025-02-06
+ * UpdateReason: 实现异步搜索支持，以避免主线程阻塞
+ * Description: 搜索桥接类的实现，用于处理搜索请求和管理搜索历史
+ */
+
 #include "SearchBridge.h"
 #include "../../log/Logger.h"
 #include <QDebug>
@@ -7,6 +15,12 @@
 
 namespace IntelliSearch {
 
+/*
+ * Summary: SearchBridge类构造函数
+ * Parameters:
+ *   QObject* parent - 父对象指针
+ * Description: 初始化搜索桥接类，设置数据库管理器和异步搜索完成的信号处理
+ */
 SearchBridge::SearchBridge(QObject* parent)
     : QObject(parent)
     , intentParser(std::make_unique<IntentParser>())
@@ -42,6 +56,13 @@ SearchBridge::SearchBridge(QObject* parent)
 
 SearchBridge::~SearchBridge() = default;
 
+/*
+ * Summary: 处理搜索请求
+ * Parameters:
+ *   const QString& query - 搜索查询字符串
+ * Return: void
+ * Description: 异步处理搜索请求，解析搜索意图并返回结果
+ */
 void SearchBridge::handleSearch(const QString& query) {
     INFOLOG("Received search request: {}", query.toStdString());
     lastQuery = query;  // 保存查询字符串供后续使用
@@ -88,6 +109,12 @@ void SearchBridge::handleSearch(const QString& query) {
     searchWatcher.setFuture(future);
 }
 
+/*
+ * Summary: 获取搜索历史记录
+ * Parameters: 无
+ * Return: QVariantList - 包含搜索历史记录的列表
+ * Description: 从数据库获取最近10条搜索历史记录
+ */
 QVariantList SearchBridge::getSearchHistory() {
     INFOLOG("Getting search history records");
     QVariantList historyList;
